1e6795c634dbe04a1fe1692c0aaa2c84a37bbc29
Madhur Gupta <madhurgupta107@gmail.com>
2023-05-07 10:34:45 +0530

    Add enumerable token ids to Forest contract

--- hardhat/contracts/Forest.sol
+++ hardhat/contracts/Forest.sol
@@ -2,23 +2,46 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract Forest is ERC721, ERC721URIStorage {
+contract Forest is ERC721, ERC721Enumerable, ERC721URIStorage {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
     constructor() ERC721("Forest", "FOR") {}
+
+    /*
+        - This function is used to mint a new NFT (non-fungible token) and assign it to the caller's address.
+        - It takes two parameters: `tokenId` and `uri`. The `tokenId` is a unique identifier for the new NFT, and the `uri` is a string that represents the metadata associated with the NFT.
+    */
+    function safeMint(string memory uri) public {
+        require(
+            // caller should not have a token
+            balanceOf(msg.sender) == 0,
+            "ERC721: caller already has a token"
+        );
+
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+
+        _safeMint(msg.sender, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
 
     /*
         - This function is checking if the caller already owns an NFT (non-fungible token) by calling the `balanceOf()` function to get the number of tokens owned by the caller.
         - If the balance is not equal to 0, it will revert with an error message.
     */
-    function checkIfUserHasNFT() public view returns (uint256[] memory) {
+    function getTokenId() public view returns (uint256) {
         require(
-            balanceOf(msg.sender) == 0,
+            // caller should have a token
+            balanceOf(msg.sender) > 0,
             "ERC721: caller does not have a token"
         );
 
-        uint256[] memory tokenId;
-        tokenId = tokenOfOwnerByIndex(msg.sender, 0);
+        uint256 tokenId = tokenOfOwnerByIndex(msg.sender, 0);
 
         return tokenId;
     }
@@ -37,27 +60,14 @@ contract Forest is ERC721, ERC721URIStorage {
     }
 
     /*
-        - This function is used to mint a new NFT (non-fungible token) and assign it to the caller's address.
-        - It takes two parameters: `tokenId` and `uri`. The `tokenId` is a unique identifier for the new NFT, and the `uri` is a string that represents the metadata associated with the NFT.
-    */
-    function safeMint(uint256 tokenId, string memory uri) public {
-        require(
-            balanceOf(msg.sender) < 1,
-            "ERC721: caller already has a token"
-        );
-
-        _safeMint(msg.sender, tokenId);
-        _setTokenURI(tokenId, uri);
-    }
-
-    /*
         - A public function that allows the owner or an approved address to modify the URI associated with a specific token.
         - It takes the token ID and the new URI as inputs and uses the internal '_setTokenURI' function to update the token URI.
         - If the caller is not the owner nor approved, it reverts with an error message.
     */
     function modifyTokenURI(uint256 tokenId, string memory uri) public {
         require(
-            _isApprovedOrOwner(_msgSender(), tokenId),
+            // caller should be owner or approved
+            _isApprovedOrOwner(msg.sender, tokenId),
             "ERC721: caller is not owner nor approved"
         );
         _setTokenURI(tokenId, uri);
@@ -65,15 +75,32 @@ contract Forest is ERC721, ERC721URIStorage {
 
     // The following functions are overrides required by Solidity.
 
-    function _burn(
-        uint256 tokenId
-    ) internal override(ERC721, ERC721URIStorage) {
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId, batchSize);
+    }
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
         super._burn(tokenId);
     }
 
-    function tokenURI(
-        uint256 tokenId
-    ) public view override(ERC721, ERC721URIStorage) returns (string memory) {
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
         return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
     }
 }
