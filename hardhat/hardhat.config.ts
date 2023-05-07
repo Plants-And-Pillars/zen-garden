3ecc81fa848a0d976b4e96dafe545c190f04b1ef
Madhur Gupta <madhurgupta107@gmail.com>
2023-05-07 11:23:45 +0530

    Fix hardhat config and deploy script

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -1,8 +1,15 @@
+import "dotenv/config";
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.18",
+  solidity: "0.8.9",
+  networks: {
+    polygon_mumbai: {
+      url: "https://rpc-mumbai.maticvigil.com",
+      accounts: [process.env.PRIVATE_KEY!],
+    },
+  },
 };
 
 export default config;
